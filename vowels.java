ecd2fdd9d81d52ef663b41c67338f2fbd8d41545
Akshayaperumal <akshayaperumal1996@gmail.com>
2017-06-08 09:22:38 +0530

    Create vowels.java

---
+++ vowels.java
@@ -0,0 +1,12 @@
+# vowels.java
+import java.io.*;
+import java.util.*;
+public class vowels
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+String a=s.next();
+
+}
+}
